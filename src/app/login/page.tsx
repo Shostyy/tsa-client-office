352f1c67461105c1bd9a8751045ad53804d75763
Shostyy <anatoliy.shostal@gmail.com>
2025-12-02 20:21:10 +0200

    Init and some stuff

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggle } from "@/components/common";
+import { Button } from "@/components/ui/button";
+
+const Login = () => {
+  return (
+    <>
+      <ThemeToggle />
+      <Button variant={"outline"}>asd</Button>
+    </>
+  );
+};
+
+export default Login;
